b5317f503293ddb2b4afbd060cd22ebcbdaad545
IKorun <korunny@mail.com>
2015-08-28 18:02:03 +0300

    FirstPush

---
+++ Assets/DeletingScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeletingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider myTrigger) 
+	{
+		if (myTrigger.gameObject.name == "Souls" || myTrigger.gameObject.name == "Souls(Clone)")
+		{
+			Destroy(myTrigger.gameObject, 0.1f);
+		}
+	}
+}
---
+++ Assets/SpawnScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	public GameObject thePrefab;
+	public int maxRandomDigit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		System.Random rnd = new System.Random();
+		int randomChance = rnd.Next(maxRandomDigit);
+
+		if (randomChance == 0) {
+			GameObject instance =  Instantiate(thePrefab, 
+			                       thePrefab.transform.position, 
+			                       thePrefab.transform.rotation) as GameObject; 
+		}
+	}
+}
