3c74b6e658736c193b06a9e49831742c2fad8fbf
IKorun <korunny@mail.com>
2015-08-29 16:08:12 +0300

    Fix

--- Assets/Scripts/DeletingScript.cs
+++ Assets/Scripts/DeletingScript.cs
@@ -13,24 +13,24 @@ public class DeletingScript : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider myTrigger) {
-		myTrigger.gameObject.GetComponent<DragNDrop> ().IsMouseTriger = true;
 		GameObject triggerObj = myTrigger.gameObject;
+		triggerObj.GetComponent<DragNDrop> ().IsMouseTriger = true;
 
 		if (triggerObj.name == "Souls" || triggerObj.name == "Souls(Clone)") {
-			Rigidbody rigidbody = myTrigger.gameObject.GetComponent<Rigidbody>();
-			GameObject obj = GameObject.Find("Main Camera");
+			Rigidbody rigidbody = triggerObj.GetComponent<Rigidbody>();
+			GameObject camera = GameObject.Find("Main Camera");
 
 			if (name == "Hell") {
 				rigidbody.AddForce(new Vector3(0, -500f, 0));
-				obj.GetComponent<StatisticScript>().addSoulToHell(triggerObj);
+				camera.GetComponent<StatisticScript>().addSoulToHell(triggerObj);
 			}
 
 			if (name == "Heaven") {
 				rigidbody.AddForce(new Vector3(0, 500f, 0));
-				obj.GetComponent<StatisticScript>().addSoulToHeaven(triggerObj);
+				camera.GetComponent<StatisticScript>().addSoulToHeaven(triggerObj);
 			}
 
-			Destroy(myTrigger.gameObject, 5.0f);
+			Destroy(triggerObj, 5.0f);
 		}
 	}
 }
