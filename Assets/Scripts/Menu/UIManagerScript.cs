b5b6b7a1172b428cea2884c72cefab40ad9405c0
melistrate95 <misha95list@gmail.com>
2015-09-28 23:05:33 +0300

    Add animation to the menu and dialog settings.

--- Assets/Scripts/Menu/UIManagerScript.cs
+++ Assets/Scripts/Menu/UIManagerScript.cs
@@ -3,11 +3,28 @@ using System.Collections;
 
 public class UIManagerScript : MonoBehaviour {
 
+	public Animator startButton;
+	public Animator settingsButton;
+	public Animator settingDialog;
+
 	public void StartGame() {
-		Application.LoadLevel("GameScene");
+		Application.LoadLevel ("GameScene");
 	}
 
 	public void ExitGame() {
 		Application.Quit();
+	}
+
+	public void OpenSettings() {
+		startButton.SetBool ("isHidden", true);
+		settingsButton.SetBool ("isHidden", true);
+		settingDialog.enabled = true;
+		settingDialog.SetBool ("isHidden", false);
+	}
+
+	public void CloseSettings() {
+		settingDialog.SetBool ("isHidden", true);
+		startButton.SetBool ("isHidden", false);
+		settingsButton.SetBool ("isHidden", false);
 	}
 }
